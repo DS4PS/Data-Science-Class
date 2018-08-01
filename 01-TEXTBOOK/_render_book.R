4573444c6189f2764a3c325299252aa8b1e01906
Jesse Lecy <jdlecy@gmail.com>
2018-08-01 12:08:05 -0700

    updates to text format

--- 01-TEXTBOOK/_render_book.R
+++ 01-TEXTBOOK/_render_book.R
@@ -2,7 +2,8 @@ library( "rmarkdown" )
 pandoc_version()
 
 library( "bookdown" )
-setwd( "C:/Users/jdlecy/Dropbox/00 - PEDA Program Design/00 - R Book/test" )
+setwd( "C:/Users/jdlecy/Dropbox/00 - PEDA Program Design/00 - R Book/Data-Science-Class/01-TEXTBOOK" )
+
 
 bookdown::render_book( "index.Rmd", "bookdown::gitbook" )
 bookdown::render_book( 'index.Rmd', 'bookdown::pdf_book' )
---
+++ 01-TEXTBOOK/_rmarkdown_settings.R
@@ -0,0 +1,17 @@
+knitr::opts_chunk$set(
+  warning = FALSE,
+  message = FALSE,
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
+
+library( dplyr )
+library( pander )
+
+options( dplyr.print_min = 6, dplyr.print_max = 6 )
